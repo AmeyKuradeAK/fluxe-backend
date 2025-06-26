c3435bd76d77c40c3f8c9169f9b32e31506e495e
Ammey Kuraaday <ameykurade60@gmail.com>
2025-06-26 23:11:29 +0530

    Fix import path for apiKeyAuth middleware by changing filename case from 'apiKeyAuth' to 'apikeyAuth' in app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const apiKeyAuth = require('./middleware/apiKeyAuth');
+const apiKeyAuth = require('./middleware/apikeyAuth');
 
 dotenv.config();
 
